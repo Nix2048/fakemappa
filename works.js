433d44779bf7ebb2eaa40f88f925b55c86af9582
Nix2048 <cn2p4cv8sy@privaterelay.appleid.com>
2026-02-17 20:11:00 +0700

    Add files via upload

---
+++ works.js
@@ -0,0 +1,7 @@
+let nav = document.querySelector(".navScreen");
+let Bar = document.querySelector(".menu-bar");
+
+function showMenu() {
+	nav.classList.toggle("active");
+	Bar.classList.toggle("active");
+}
